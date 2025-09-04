0c0612b05d8cfcfb01c2fb477a4fa8823ac6c460
Gary Rojas <naruto13581@gmail.com>
2025-09-04 16:47:07 -0400

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace Names
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnAdd = new Button();
+            label1 = new Label();
+            firstNames = new ListBox();
+            txtName = new TextBox();
+            SuspendLayout();
+            // 
+            // btnAdd
+            // 
+            btnAdd.Location = new Point(138, 55);
+            btnAdd.Name = "btnAdd";
+            btnAdd.Size = new Size(100, 23);
+            btnAdd.TabIndex = 0;
+            btnAdd.Text = "Add Name";
+            btnAdd.UseVisualStyleBackColor = true;
+            btnAdd.Click += btnAdd_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(44, 15);
+            label1.TabIndex = 1;
+            label1.Text = "Names";
+            label1.Click += label1_Click;
+            // 
+            // firstNames
+            // 
+            firstNames.FormattingEnabled = true;
+            firstNames.Location = new Point(12, 27);
+            firstNames.Name = "firstNames";
+            firstNames.Size = new Size(120, 94);
+            firstNames.TabIndex = 2;
+            firstNames.SelectedIndexChanged += listBox1_SelectedIndexChanged;
+            // 
+            // txtName
+            // 
+            txtName.Location = new Point(138, 26);
+            txtName.Name = "txtName";
+            txtName.Size = new Size(100, 23);
+            txtName.TabIndex = 3;
+            txtName.TextChanged += textBox1_TextChanged;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(252, 141);
+            Controls.Add(txtName);
+            Controls.Add(firstNames);
+            Controls.Add(label1);
+            Controls.Add(btnAdd);
+            Name = "Form1";
+            Text = "Names";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button btnAdd;
+        private Label label1;
+        private ListBox firstNames;
+        private TextBox txtName;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,31 @@
+namespace Names
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(txtName.Text) && !firstNames.Items.Contains(txtName.Text))
+                firstNames.Items.Add(txtName.Text);
+        }
+    }
+}
